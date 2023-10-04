57ab73cca6219f9ea586c6d431156414d08edc36
PatrickLBrock <patrickleonard0312@gmail.com>
2023-10-04 14:42:54 -0400

    i am a dumby and stupid

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableMotionStates/PlayerWalkMotionState.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableMotionStates/PlayerWalkMotionState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// </summary>
 public class PlayerWalkMotionState : PlayerBasicMotionState
 {
-    private const float WALK_SPEED = 4.5f;
+    private const float WALK_SPEED = 12f;//12 for regular walk, 16 for run, 6 for crouch, sliding 18-20
 
     public override void stateUpdate()
     {
