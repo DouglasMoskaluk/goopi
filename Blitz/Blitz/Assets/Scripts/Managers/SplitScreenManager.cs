77adaf1195dc06c7c3e80ce934d8ec98ea14a247
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-11-01 14:29:26 -0400

    get player id and static ref

--- Blitz/Blitz/Assets/Scripts/Managers/SplitScreenManager.cs
+++ Blitz/Blitz/Assets/Scripts/Managers/SplitScreenManager.cs
@@ -5,31 +5,45 @@ using UnityEngine.InputSystem;
 using Cinemachine;
 
 public class SplitScreenManager : MonoBehaviour
+{
+    private List<PlayerInput> players = new List<PlayerInput>();
+
+    [SerializeField]
+    private List<LayerMask> playerLayers;
+
+    private PlayerInputManager inputManager;
+
+    public static SplitScreenManager instance;
+
+    private void Awake()
     {
-        private List<PlayerInput> players = new List<PlayerInput>();
-
-        [SerializeField]
-        private List<LayerMask> playerLayers;
-
-        private PlayerInputManager inputManager;
-
-        private void Awake()
-        {
-            inputManager = FindObjectOfType<PlayerInputManager>();
-            inputManager.onPlayerJoined += AddPlayer;
-        }
-
-        public void AddPlayer(PlayerInput player)
-        {
-            players.Add(player);
- 
-            int layerToAdd = (int)Mathf.Log(playerLayers[players.Count - 1].value, 2);
-
-            player.transform.GetComponentInChildren<CinemachineFreeLook>().gameObject.layer = layerToAdd;
-            //add the layer
-            player.transform.GetChild(0).GetChild(0).GetComponent<Camera>().cullingMask |= 1 << layerToAdd;
-
-            player.transform.GetComponentInChildren<PlayerCamInput>().lookValue = player.actions.FindAction("Look");
-
-        }
+        if (instance == null) instance = this; 
+        inputManager = FindObjectOfType<PlayerInputManager>();
+        inputManager.onPlayerJoined += AddPlayer;
     }
+
+    public void AddPlayer(PlayerInput player)
+    {
+        players.Add(player);
+ 
+        int layerToAdd = (int)Mathf.Log(playerLayers[players.Count - 1].value, 2);
+
+        player.transform.GetComponentInChildren<CinemachineFreeLook>().gameObject.layer = layerToAdd;
+        //add the layer
+        player.transform.GetChild(0).GetChild(0).GetComponent<Camera>().cullingMask |= 1 << layerToAdd;
+
+        player.transform.GetComponentInChildren<PlayerCamInput>().lookValue = player.actions.FindAction("Look");
+    }
+
+    public int getPlayerID(GameObject player)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (player == players[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
