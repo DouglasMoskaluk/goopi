0b9ea7e85139c6a582519af2afef097086300224
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-11-22 21:47:02 -0500

    gun spawns rotated semi correctly need to alter the gun prefabs to account for their slight changes in rotation and positions needed but it looks okay enough rn

--- Blitz/Blitz/Assets/Scripts/Managers/GunManager.cs
+++ Blitz/Blitz/Assets/Scripts/Managers/GunManager.cs
@@ -15,23 +15,29 @@ public class GunManager : MonoBehaviour
     void Awake()
     {
         if (instance == null) instance = this;
+        
+    }
+
+    private void Start()
+    {
         changeGuns();
     }
 
     internal void changeGuns()
     {
         gunUsed = Random.Range(0, guns.Length - 1);
-        for (int i=0; i< SplitScreenManager.instance.GetPlayers().Count; i++)
+        for (int i=0; i < SplitScreenManager.instance.GetPlayers().Count; i++)
         {
-            assignGun(i);
+            assignGun(gunUsed);
         }
     }
 
     internal void assignGun(int Player)
     {
         Transform plr = SplitScreenManager.instance.GetPlayers()[Player].transform;
-        if (plr.GetComponent<PlayerBodyFSM>().playerGun.gameObject != null) Destroy(plr.GetComponent<PlayerBodyFSM>().playerGun.gameObject);
-        GameObject gun = Instantiate(guns[gunUsed], new Vector3(0.3f, 1, 0), plr.rotation, plr.GetChild(1));
+        PlayerBodyFSM FSM = plr.GetComponent<PlayerBodyFSM>();
+        if (FSM.playerGun.gameObject != null) Destroy(FSM.playerGun.gameObject);
+        GameObject gun = Instantiate(guns[gunUsed], new Vector3(0.3f, 1, 0), FSM.playerBody.rotation, plr.GetChild(1));
         gun.GetComponent<Gun>().gunVars.bulletParent = transform;
         plr.GetComponent<PlayerBodyFSM>().assignGun(gun);
     }
