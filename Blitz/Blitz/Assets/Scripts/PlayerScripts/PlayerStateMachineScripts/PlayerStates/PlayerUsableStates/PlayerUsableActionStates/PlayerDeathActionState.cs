0bb4626c6c72460551c241a12cfd2c50e9bef246
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-12-02 11:37:48 -0500

    death states

---
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableActionStates/PlayerDeathActionState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathActionState : PlayerActionState
+{
+    public override void onStateEnter()
+    {
+        base.onStateEnter();
+    }
+
+    public override void onStateExit()
+    {
+        base.onStateEnter();
+    }
+
+    public override void stateUpdate()
+    {
+        base.onStateEnter();
+    }
+}
---
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableMotionStates/PlayerDeathMotionState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathMotionState : PlayerBasicMotionState
+{
+    public override void onStateEnter()
+    {
+        base.onStateEnter();
+    }
+
+    public override void onStateExit()
+    {
+        base.onStateEnter();
+    }
+
+    public override void stateUpdate()
+    {
+        base.onStateEnter();
+    }
+}
