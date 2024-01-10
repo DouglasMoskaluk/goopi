3e18a54178e873f747471a0ecad79d5ebf87f7a4
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-01-10 14:13:52 -0500

    grenadespawner

---
+++ Blitz/Blitz/Assets/Scripts/Grenade/GrenadePickUp.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadePickUp : MonoBehaviour
+{
+    [SerializeField] private GameObject grenadeVisual;
+    [SerializeField] private float respawnTime;
+    private bool canPickUp = true;
+
+
+    private void Start()
+    {
+        EventManager.instance.addListener(Events.onRoundStart, ResetGrenade);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.transform.CompareTag("Player") || !canPickUp) return;
+        
+        PlayerGrenadeThrower grenThrow = other.transform.GetComponent<PlayerGrenadeThrower>();
+
+        if (grenThrow == null) return;
+
+        if (grenThrow.HeldGrenadeCount >= grenThrow.MaxHeldGrenades) return;
+
+        grenThrow.addGrenade(1);
+
+        canPickUp = false;
+        grenadeVisual.SetActive(false);
+
+    }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        ResetGrenade();
+
+    }
+
+    public void ResetGrenade()
+    {
+        canPickUp = true;
+        grenadeVisual.SetActive(true);
+    }
+}
