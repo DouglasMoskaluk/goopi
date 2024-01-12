7a458df7ed1988c73036f36639205ffbc80fe70e
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-01-12 11:43:34 -0500

    better knockback

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableMotionStates/PlayerKnockBackMotionState.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerStateMachineScripts/PlayerStates/PlayerUsableStates/PlayerUsableMotionStates/PlayerKnockBackMotionState.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerKnockBackMotionState : PlayerBasicMotionState
 {
+
+    float lateralRightSpeed = 6f;
+    float lateralLeftSpeed = 6f;
+
+    float forwardSpeed = 6f;
+    float backwardSpeed = 6f;
+
+    float timeForControl = 0.5f;
+    float elapsedTime = 0f;
     public override void onStateEnter()
     {
         base.onStateEnter();
@@ -20,9 +29,27 @@ public class PlayerKnockBackMotionState : PlayerBasicMotionState
     {
         base.stateUpdate();
 
+        elapsedTime += Time.deltaTime;
+
         //potentially some movement for in air that less strong than normal in air movement, idk yet
 
-        controller.Move(FSM.getKnockBackVector() * Time.deltaTime);
+        Vector3 inputMotion = Vector3.zero;
+        Vector3 lateralMotion = Vector3.zero;
+        Vector3 forwardMotion = Vector3.zero;
+        if (elapsedTime >= timeForControl)
+        {
+            lateralMotion = (playerBody.right * input.motionInput.x);
+            forwardMotion = (playerBody.forward * input.motionInput.y);
+
+            lateralMotion = (input.motionInput.x < 0) ? lateralMotion * lateralRightSpeed : lateralMotion * lateralLeftSpeed;
+            forwardMotion = (input.motionInput.y < 0) ? forwardMotion * forwardSpeed : forwardMotion * backwardSpeed;
+
+            inputMotion = forwardMotion + lateralMotion;
+        }
+        
+
+        controller.Move( (FSM.getKnockBackVector() + inputMotion) * Time.deltaTime );
+       
         RotateBodyToCamera();
         updateKnockBack();
     }
