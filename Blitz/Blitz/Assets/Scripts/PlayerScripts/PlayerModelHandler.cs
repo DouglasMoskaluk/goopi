1da4dd8589d5d27816bf9d56083bf3d7d07ec6d6
PatrickLBrock <patrickleonard0312@gmail.com>
2024-01-17 02:43:40 -0500

    character select working

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerModelHandler.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerModelHandler.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerModelChanger : MonoBehaviour
 {
     [SerializeField]
     private SkinnedMeshRenderer playerSkin;
+
+    [SerializeField]
+    private GameObject[] headFeatures;
+
+    [SerializeField]
+    private Mesh[] characterMesh;
+
+    [SerializeField]
+    private Material[] characterMaterial;
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +23,23 @@ public class PlayerModelChanger : MonoBehaviour
         
     }
 
-    public void SetModel(Mesh newMesh, Material newMaterial)
+    public void SetModel(int featureNum)
     {
-        playerSkin.sharedMesh = newMesh;
-        playerSkin.sharedMaterial = newMaterial;
+
+        for(int i = 0; i < headFeatures.Length; i++)
+        {
+            if(featureNum == i)
+            {
+                headFeatures[i].SetActive(true);
+                playerSkin.sharedMesh = characterMesh[i];
+                playerSkin.sharedMaterial = characterMaterial[i];
+            }
+            else
+            {
+                headFeatures[i].SetActive(false);
+            }
+        }
+
     }
 
     // Update is called once per frame
