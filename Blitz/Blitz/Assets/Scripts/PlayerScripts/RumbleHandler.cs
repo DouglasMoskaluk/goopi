abedbf27c3652c58bfb685114524c04e06d5328b
PatrickLBrock <patrickleonard0312@gmail.com>
2024-01-18 21:34:55 -0500

    updated rumble
    fixed bugs and set it up for xbox and ps controllers

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/RumbleHandler.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/RumbleHandler.cs
@@ -2,9 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+using UnityEngine.InputSystem.XInput;
 
 public class RumbleHandler : MonoBehaviour
 {
+
+    private Gamepad playerGamepad;
+
+    private void Awake()
+    {
+        if (GetComponent<PlayerInput>().devices[0] is XInputController or DualShockGamepad)
+        {
+            playerGamepad = (Gamepad)GetComponent<PlayerInput>().devices[0];
+        }
+        else
+        {
+            Debug.Log("KEYBOARD OR MOUSE");
+            playerGamepad = null;
+        }
+    }
+
     /// <summary>
     /// to call shoot co routine
     /// </summary>
@@ -13,24 +31,76 @@ public class RumbleHandler : MonoBehaviour
         switch(gunType)
         {
             case 0:
+                if(playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.02f));
+                }
+                else if(playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
                 break;
             case 1:
+                if (playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.015f));
+                }
+                else if (playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
                 break;
             case 2:
+                if (playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.015f));
+                }
+                else if (playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
+                break;
+            case 3:
+                if (playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.015f));
+                }
+                else if (playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
+                break;
+            case 4:
+                if (playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.015f));
+                }
+                else if (playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
+                break;
+            case 5:
+                if (playerGamepad is XInputController)
+                {
+                    StartCoroutine(gunRumble(0.05f, 0.015f));
+                }
+                else if (playerGamepad is DualShockGamepad)
+                {
+                    StartCoroutine(gunRumble(0.005f, 0.02f));
+                }
                 break;
         }
-
-        StartCoroutine(gunRumble());
     }
 
     /// <summary>
     /// coroutine to rumble on shoot
     /// </summary>
-    IEnumerator gunRumble()
+    IEnumerator gunRumble(float motorValue, float rumbleLength)
     {
-        Gamepad.current.SetMotorSpeeds(0.0f, 0.005f);
-        yield return new WaitForSeconds(0.02f);
-        Gamepad.current.SetMotorSpeeds(0f, 0f);
+        playerGamepad.SetMotorSpeeds(0.0f, motorValue);
+        yield return new WaitForSeconds(rumbleLength);
+        playerGamepad.SetMotorSpeeds(0f, 0f);
     }
 
 }
