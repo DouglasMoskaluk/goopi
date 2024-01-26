9396fcb16276afc0deb595ba3c4dd7ee3f2888a0
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-01-26 12:44:47 -0500

    spine stuff

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/RotateSpineWithCamera.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/RotateSpineWithCamera.cs
@@ -8,80 +8,20 @@ public class RotateSpineWithCamera : MonoBehaviour
     [SerializeField] private Transform cam;
     [SerializeField] private Transform rotBone;
     [SerializeField] private Transform forwardDirectionTransform;
-
+    [SerializeField] private Animator anim;
 
     private Vector3 initCamRot;
+    [Range(0,1)] public float angleTest = 0.5f;
 
-    private void Start()
+    private void Awake()
     {
         initCamRot = cam.localEulerAngles;
     }
 
-    private void LateUpdate()
-    {
-        float XCamRotDiff = cam.eulerAngles.x - initCamRot.x;
-
-        Vector3 newRot = rotBone.eulerAngles;
-        newRot.x = initCamRot.x + XCamRotDiff;
-        rotBone.localEulerAngles = newRot;
+    private void LateUpdate() { 
+    
+        anim.Play("SpineRotate", 2, angleTest);
     }
-
-    //[SerializeField] private Transform cam;
-    ////[SerializeField] private Transform forwardDirectionRot;
-    //[SerializeField] private float upClamp = 25f;
-    //[SerializeField] private float downClamp = 25f;
-    //private Vector3 camInitEuler;
-    //private Quaternion camInitRot;
-
-    //[SerializeField] private List<WeightedBones> RotationBones;
-
-    //private void Awake()
-    //{
-    //    camInitEuler = cam.localEulerAngles;
-    //    camInitRot = cam.localRotation;
-    //    SetUpBones();
-    //}
-
-    //private void LateUpdate()
-    //{
-
-    //    float camXRotDiff = cam.eulerAngles.x - camInitEuler.x;
-    //    Debug.Log(camXRotDiff);
-    //    foreach (var rotBone in RotationBones)
-    //    {
-    //        rotBone.bone.localRotation = rotBone.initRot * Quaternion.Euler((camXRotDiff * rotBone.weight), 0, 0);
-    //        rotBone.bone.localEulerAngles = ClampX(rotBone.bone.localEulerAngles, downClamp, upClamp);
-    //    }
-    //}
-
-    //private void SetUpBones()
-    //{
-    //    float totalWeight = 0;
-    //    foreach (var rotBone in RotationBones)
-    //    {
-    //        totalWeight += rotBone.weight;
-    //    }
-
-    //    foreach (var rotBone in RotationBones)
-    //    {
-    //        rotBone.weight = rotBone.weight / totalWeight;
-    //        rotBone.initRot = rotBone.bone.localRotation;
-    //    }
-    //}
-
-    //private Vector3 ClampX(Vector3 euler, float downClamp, float upClamp)
-    //{
-    //    if (euler.x > upClamp) { euler.x = upClamp; }
-
-    //    return euler;
-    //}
-}
-
-public enum ClampAxis
-{
-    X = 0,
-    Y = 1,
-    Z = 2
 }
 
 [Serializable]
