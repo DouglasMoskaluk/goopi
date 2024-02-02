29104f6c17b06b29601b3db96d9ad802e834c0de
PatrickLBrock <patrickleonard0312@gmail.com>
2024-02-02 12:12:46 -0500

    bug fex

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerUIHandler.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerUIHandler.cs
@@ -88,8 +88,8 @@ public class PlayerUIHandler : MonoBehaviour
         lowHealthUI.SetActive(false);
         damagedUI.SetActive(false);
         crossHair.SetActive(false);
-        characterChoice.SetActive(true);
-        eventhandler.SetSelectedGameObject(charButtons[playerID]);
+
+        StartCoroutine(setCharButtons());
     }
 
     private void Initialize()
@@ -228,6 +228,13 @@ public class PlayerUIHandler : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
         damagedUI.SetActive(false);
         yield return null;
+    }
+
+    IEnumerator setCharButtons()
+    {
+        yield return new WaitForEndOfFrame();
+        characterChoice.SetActive(true);
+        eventhandler.SetSelectedGameObject(charButtons[playerID]);
     }
 
 }
