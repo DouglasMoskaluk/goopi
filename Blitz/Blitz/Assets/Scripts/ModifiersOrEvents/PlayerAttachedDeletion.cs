465b03f2605082455693e63318413c9fe6dd3edd
PatrickLBrock <patrickleonard0312@gmail.com>
2024-02-05 02:31:22 -0500

    skybeam doesnt render on player cam

--- Blitz/Blitz/Assets/Scripts/ModifiersOrEvents/PlayerAttachedDeletion.cs
+++ Blitz/Blitz/Assets/Scripts/ModifiersOrEvents/PlayerAttachedDeletion.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerAttachedDeletion : MonoBehaviour
 {
     int plr = -1;
+
+    [SerializeField]
+    private List<LayerMask> renderLayers;
 
     void deathCheck(EventParams param = new EventParams())
     {
@@ -23,6 +27,8 @@ public class PlayerAttachedDeletion : MonoBehaviour
     internal void init(int id)
     {
         plr = id;
+        int layerToAdd = (int)Mathf.Log(renderLayers[plr].value, 2);
+        transform.gameObject.layer = layerToAdd;
         EventManager.instance.addListener(Events.onPlayerDeath, deathCheck);
         EventManager.instance.addListener(Events.onRoundEnd, roundEnd);
         //int bitmask = (1 << (11 + plr));
