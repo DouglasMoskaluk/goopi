3eac59b9f746d839b72699f320ade768f126d314
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-02-07 13:49:33 -0500

    start of tail ik

---
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerTailMotion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTailMotion : MonoBehaviour
+{
+    [SerializeField] private PlayerInputHandler input;
+    
+
+    private void LateUpdate()
+    {
+        
+    }
+
+}
