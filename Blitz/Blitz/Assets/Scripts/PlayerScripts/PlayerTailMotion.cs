bf8519ca703849bb677e4980604cb6fce7088b59
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-02-26 19:49:26 -0500

    basic shitty tail ik

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerTailMotion.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerTailMotion.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class PlayerTailMotion : MonoBehaviour
 {
     [SerializeField] private PlayerInputHandler input;
-    
+
+    [SerializeField] private Transform tailIKTarget;
+
+    [SerializeField] private SplineContainer tailIKSpline;
+
+    [SerializeField, Range(0,1)] private float TEST_PlaceTailAt;
 
     private void LateUpdate()
     {
-        
+        float3 tail = tailIKSpline.EvaluatePosition(TEST_PlaceTailAt);
+        tailIKTarget.position = new Vector3(tail.x, tail.y, tail.z);
     }
 
 }
