087ae8b9e6c3c0eee87e4d372c890d668a9494d1
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2024-02-28 16:07:49 -0500

    stuff

--- Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerHeadMotion.cs
+++ Blitz/Blitz/Assets/Scripts/PlayerScripts/PlayerHeadMotion.cs
@@ -19,7 +19,15 @@ public class PlayerHeadMotion : MonoBehaviour
         Vector3 input = inputHandler.motionInput;
  
 
-        transform.rotation = ogRotation * Quaternion.AngleAxis(-rotationAmount * input.y, rotationDirections.right) //y
+        transform.localRotation = ogRotation * Quaternion.AngleAxis(-rotationAmount * input.y, rotationDirections.right) //y
             * Quaternion.AngleAxis(rotationAmount * input.x, rotationDirections.forward); //x
+    }
+
+    private float easeOutBack(float num)
+    {
+        const float c1 = 1.70158f;
+        const float c3 = c1 + 1;
+
+        return 1 + c3 * Mathf.Pow(num - 1, 3) + c1 * Mathf.Pow(num - 1, 2);
     }
 }
