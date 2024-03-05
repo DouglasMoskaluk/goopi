cc88f95f2a22f0c5e601b97e04e6d16358752e0c
PatrickLBrock <patrickleonard0312@gmail.com>
2024-03-05 15:45:28 -0500

    starting bullet decal

---
+++ Blitz/Blitz/Assets/Scripts/Gun/BulletDecal.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDecal : MonoBehaviour
+{
+
+    [SerializeField]
+    private float lifetime = 5.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        EventManager.instance.addListener(Events.onRoundStart, RemoveSelf);
+        StartCoroutine("Countdown");
+    }
+
+    public void RemoveSelf(EventParams param = new EventParams())
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+
+}
