0a78acd0828bb8b6fa8ab56dac1d996f06c86cb8
PatrickLBrock <patrickleonard0312@gmail.com>
2024-03-15 09:36:29 -0400

    roulette tick rumble

--- Blitz/Blitz/Assets/Scripts/UIScripts/RouletteTickSoundDetector.cs
+++ Blitz/Blitz/Assets/Scripts/UIScripts/RouletteTickSoundDetector.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RouletteTickSoundDetector : MonoBehaviour
 {
+    private List<RumbleHandler> playerRumbles;
+
     public void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("UIRouletteSpinner"))
         {
             AudioManager.instance.PlaySound(AudioManager.AudioQueue.ROULETTE_SPIN);
+            //play rumble on all controllers
+            List<PlayerInput> newPlayerInputList = SplitScreenManager.instance.GetPlayers();
+            for (int i = 0; i < newPlayerInputList.Count; i++)
+            {
+                newPlayerInputList[i].transform.GetComponent<RumbleHandler>().RumbleTick();
+            }
         }
     }
+
 }
